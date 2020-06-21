0a0dd296113928298d7873a4cb4c64ff31c999d6
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-06-21 13:29:53 +0400

    Initial commit

---
+++ app/src/main/java/com/example/android/homework9/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.homework9;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String NOTE_TEXT = "note_text";
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+    private SharedPreferences myNoteSharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        getDateFromSharedPref();
+
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTxt = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTxt);
+                myEditor.apply();
+                Toast.makeText(MainActivity.this, R.string.saved_data, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getDateFromSharedPref(){
+        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
+        mInputNote.setText(noteTxt);
+    }
+
+    private void initViews() {
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+        myNoteSharedPref = getSharedPreferences(getString(R.string.my_note), MODE_PRIVATE);
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+}
